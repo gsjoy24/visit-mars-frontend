79ef364bb6b4626a6451800edb4e0d1d3b7ca1f3
gsjoy24 <gour.joy24@gmail.com>
2024-07-27 01:56:44 +0600

    updated application count

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -65,7 +65,7 @@ const Visitors = () => {
 							{visitors.map((row, i) => (
 								<TableRow key={row?.fullName} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
 									<TableCell component='th' scope='row'>
-										{i}
+										{i + 1}
 									</TableCell>
 									<TableCell component='th' scope='row'>
 										{row?.fullName}
